724a9d128e24a01a719591317a0d0dc2a23813b0
shyines <matei.chira@gmail.com>
2024-10-16 07:02:31 +0300

    ,

---
+++ Anul2/Sem1/AF/Lab2/selectionSort/cmake-build-debug/CMakeFiles/selectionSort.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for selectionSort.
+# This may be replaced when dependencies are built.
--- Anul2/Sem1/AF/Lab2/selectionSort/main.cpp
+++ Anul2/Sem1/AF/Lab2/selectionSort/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Profiler.h"
 
+
+
+
 Profiler profiler;
 
 void swap(int *a, int *b) {
@@ -9,33 +12,41 @@ void swap(int *a, int *b) {
     *b = aux;
 }
 
-
-void selectionSort(int a[], int n) {
-    Operation compSelectionSort = profiler.createOperation("compSelectionSort", n);
-    Operation asgSel = profiler.createOperation("asgSel", n);
+void showArr(int a[], int n) {
     for(int i = 0;i < n; i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int selectionSort(int a[], int n) {
+    Operation compSelectionSort = profiler.createOperation("compSelectionSort", n);
+    Operation asgSelectionSort = profiler.createOperation("asgSelectionSort", n);
+    for(int i = 0;i < n - 1; i++) {
         int aux = i;
         for(int j = i + 1;j < n; j++) {
             compSelectionSort.count();
-            if(a[j] < a[i]) {
+            if(a[j] < a[aux]) {
                 aux = j;
             }
         }
         if(aux != i) {
-            asgSel.count(3);
+            asgSelectionSort.count(3);
             swap(&a[i], &a[aux]);
         }
     }
+
+    return 0;
 }
 
-void bubbleSort(int a[], int n) {
+int bubbleSort(int a[], int n) {
     Operation compBubbleSort = profiler.createOperation("compBubbleSort", n);
     Operation asgBubbleSort = profiler.createOperation("asgBubbleSort", n);
     bool sorted = false;
     int i = 0;
     do {
         sorted = true;
-        for(int j = 0;j < n - i; j++) {
+        for(int j = 0;j < n - i - 1; j++) {
             compBubbleSort.count();
             if(a[j] > a[j + 1]) {
                 asgBubbleSort.count(3);
@@ -45,49 +56,96 @@ void bubbleSort(int a[], int n) {
         }
         i++;
     }while (!sorted);
+
+    return 0;
 }
 
-void insertionSort(int a[], int n) {
+int insertionSort(int a[], int n) {
+    Operation compInsertionSort = profiler.createOperation("compInsertionSort", n);
+    Operation asgInsertionSort = profiler.createOperation("asgInsertionSort", n);
     int j = 0;
     for(int i = 1;i < n; i++) {
         int aux = a[i];
-        for(j = i - 1;j >= 0; j--) {
-            if(a[i] > a[j]) {
-                break;
-            }
+        j = i - 1;
+        compInsertionSort.count();
+        while(j >= 0 && a[j] > aux) {
+            asgInsertionSort.count();
             a[j + 1] = a[j];
+            j--;
         }
-        a[j] = aux;
+        asgInsertionSort.count();
+        a[j + 1] = aux;
     }
+
+    return 0;
 }
 
 
-void showArr(int a[], int n) {
-    for(int i = 0;i < n; i++) {
-        std::cout << a[i] << " ";
-    }
-    std::cout << std::endl;
+
+
+void demo() {
+    int arr1[] = {5, 1, 4, 20, 16};
+    int n1 = sizeof(arr1) / sizeof(int);
+
+    int arr2[] = {5, 1, 4, 20, 16};
+    int n2 = sizeof(arr2) / sizeof(int);
+
+    int arr3[] = {5, 1, 4, 20, 16};
+    int n3 = sizeof(arr3) / sizeof(int);
+
+    std::cout <<"Bubble Sort Algorithm on the arr: ";
+    showArr(arr1, n1);
+    bubbleSort(arr1, n1);
+    showArr(arr1, n1);
+
+    std::cout <<"Selection sort algorithm on the arr: ";
+    showArr(arr2, n2);
+    selectionSort(arr2, n2);
+    showArr(arr2, n2);
+
+    std::cout <<"Insertion Sort Algorithm on the arr: ";
+    showArr(arr3, n3);
+    insertionSort(arr3, n3);
+    showArr(arr3, n3);
 }
-int main() {
-    /*
-    int arr[10000];
-    int brr[10000];
-    int crr[10000];
-    for(int j = 0;j < 5; j++) {
-        for (int i = 100; i < 10000; i += 100) {
-            FillRandomArray(arr, 10000, -50000, 50000);
-            selectionSort(arr, i);
+
+
+
+void sortingAlgortihm (int (*sort[])(int arr[], int n)) {
+    int src[10000];
+    int dest[10000];
+
+    for(int i = 0 ;i < 5; i++) {
+        for(int j = 100;j <= 10000; j += 100) {
+            FillRandomArray(src, j);
+            for(int k = 0;k < 3; k++) {
+                CopyArray(dest, src, j);
+                sort[k](dest, j);
+            }
         }
     }
+
     profiler.divideValues("compSelectionSort", 5);
-    profiler.divideValues("asgSel", 5);
-    profiler.addSeries("selectionSort", "compSelectionSort", "asgSel");
+    profiler.divideValues("asgSelectionSort", 5);
+
+    profiler.divideValues("compInsertionSort", 5);
+    profiler.divideValues("asgInsertionSort", 5);
+
+    profiler.divideValues("compBubbleSort", 5);
+    profiler.divideValues("asgBubbleSort", 5);
+
     profiler.showReport();
-     */
-    int arr[5];
-    FillRandomArray(arr, 5, 10, 24);
-    int n = sizeof(arr) / sizeof(int);
-    insertionSort(arr, n);
-    showArr(arr, n);
+}
+
+
+int main() {
+    int (*fArr[3])(int arr[], int n) {
+        insertionSort, selectionSort, bubbleSort
+    };
+
+    std::cout << "Demos direct sorting methods: " << std::endl;
+    std::cout << std::endl;
+    demo();
+    sortingAlgortihm(fArr);
     return 0;
 }
