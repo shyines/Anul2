5d84dbf4e3a6a59b59ec678ccffd465f017a4159
shyines <matei.chira@gmail.com>
2024-10-22 12:03:13 +0300

    z

---
+++ Anul2/Sem1/AF/Lab3/Heap/cmake-build-release/CMakeFiles/Heap.dir/Heap.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Heap.dir/Heap.cpp.obj: \
+ D:\Apps\Docs\Facultate\Anul2\Anul2\Sem1\AF\Lab3\Heap\Heap.cpp \
+ D:\Apps\Docs\Facultate\Anul2\Anul2\Sem1\AF\Lab3\Heap\Heap.h
--- Anul2/Sem1/AF/Lab3/Heap/main.cpp
+++ Anul2/Sem1/AF/Lab3/Heap/main.cpp
@@ -4,6 +4,8 @@
 Profiler p;
 
 
+
+
 typedef struct _heap {
     int arr[10000];
     int size;
@@ -72,11 +74,11 @@ int getLeft (int index) {
     return 2 * index;
 }
 
-int getParrent(int index) {
-    return index/2;
+int getParent(int index) {
+    return (index - 1) / 2;
 }
 
-void maxHeapify(Heap *h, int index) {
+void maxHeapify(Heap *h, int index, Operation *asg, Operation *comp) {
     int largest = 0;
     int l = getLeft(index);
     int r = getRight(index);
@@ -90,39 +92,49 @@ void maxHeapify(Heap *h, int index) {
     if(r <= h->size && h->arr[r] > h->arr[largest]) {
         largest = r;
     }
+    comp->count(2);
 
     if(largest != index) {
+        asg->count(3);
         swap(&h->arr[index], &h->arr[largest]);
-        maxHeapify(h, largest);
+        maxHeapify(h, largest, asg, comp);
     }
 }
 
 void buildHeapBottomUp(Heap *h) {
+    Operation bottomUpAsg = p.createOperation("bottomUpAsg", h->size);
+    Operation bottomUpComp = p.createOperation("bottomUpComp", h->size);
     for(int i = h->size / 2; i >= 0; i--) {
-        maxHeapify(h, i);
+        maxHeapify(h, i, &bottomUpAsg, &bottomUpComp);
     }
 }
 
-void insertKey(Heap *h, int index, int key) {
+void insertKey(Heap *h, int index, int key, Operation *asg, Operation *comp) {
     if(key < h->arr[index]) {
         std::cout << "new key is smaller than current key";
+        return; // stop execution if the new key is smaller
     }
-
+    asg->count();
     h->arr[index] = key;
-    int parent = getParrent(index);
+    int parent = getParent(index);
 
     while(index > 0 && h->arr[parent] < h->arr[index]) {
+        comp->count();
+        asg->count(3);
         swap(&h->arr[index], &h->arr[parent]);
         index = parent;
+        parent = getParent(index);  // recalculate parent after swap
     }
 }
 
 
 void buildHeapTopDown(Heap *h, int arr[], int n) {
-    for(int i = 0;i < n; i++) {
+    Operation topDownAsg = p.createOperation("topDownAsg", n);
+    Operation topDownComp = p.createOperation("topDownComp", n);
+    for(int i = 0; i < n; i++) {
+        h->arr[h->size] = INT_MIN;  // Insert a dummy value
+        insertKey(h, h->size, arr[i], &topDownAsg, &topDownComp);
         h->size++;
-        h->arr[h->size] = INT_MIN;
-        insertKey(h, h->size, arr[i]);
     }
 }
 
@@ -140,23 +152,49 @@ void demo() {
     int arr[] = {4, 1, 3, 2, 16, 9, 10, 14, 8 ,7};
     int n = sizeof(arr) / sizeof(int);
 
-    h.size = 0;
-    buildHeapTopDown(&h, arr, n);
-    showArr(h.arr, h.size);
-
     CopyArray(h.arr, arr, n);
     h.size = n;
     buildHeapBottomUp(&h);
     showArr(h.arr, h.size);
+
+    h.size = 0;
+    buildHeapTopDown(&h, arr, n);
+    showArr(h.arr, h.size);
+
+
 }
 
-void perfAll() {
 
+
+void perf() {
+    int src[10000];
+    Heap h;
+    for(int i = 0;i < 5; i++) {
+        for(int j = 100;j <= 10000; j += 100) {
+            FillRandomArray(src, j);
+            CopyArray(h.arr, src, j);
+            h.size = j;
+            buildHeapBottomUp(&h);
+
+            h.size = 0;
+            buildHeapTopDown(&h, src, j);
+        }
+    }
+
+    p.divideValues("topDownAsg", 5);
+    p.divideValues("topDownComp", 5);
+
+    p.divideValues("bottomUpAsg", 5);
+    p.divideValues("bottomUpComp", 5);
+
+    p.showReport();
 }
 
 
 int main() {
     ///demo
     demo();
+    ///perf
+    perf();
     return 0;
 }
