5370b2b084bd20a4e260cfd7db6eed2f998e8a32
shyines <matei.chira@gmail.com>
2024-11-28 12:41:56 +0200

    .

--- Anul2/Sem1/AF/Lab8/OST/main.cpp
+++ Anul2/Sem1/AF/Lab8/OST/main.cpp
@@ -28,8 +28,53 @@ Bst *createBst(int left, int right, int a[]) {
     return node;
 }
 
+void prettyPrint(Bst *root, int tabs) {
+    if(root == nullptr) {
+        return;
+    }
+    for(int i = 0;i < tabs; i++) {
+        std::cout << "\t";
+    }
+    prettyPrint(root->right, tabs + 1);
+    std::cout << root->key << "\n";
+    prettyPrint(root->left, tabs + 1);
+}
+
+Bst *osSelect(Bst *root, int i) {
+    int r = 0;
+    if(root->left == nullptr) {
+        r = root->size;
+    }else {
+        r = root->left->size + 1;
+    }
+    if(r == i) {
+        return root;
+    }
+    if(r > i) {
+        return osSelect(root->left, i);
+    }
+        return osSelect(root->right, i - r);
+}
+
+void demo() {
+
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(a) / sizeof(int);
+
+    Bst *tree = createBst(1, n, a);
+    prettyPrint(tree, 0);
+    std::cout << "os Selecting 3 random keys from the previously built tree \n";
+    for(int i = 0;i < 3; i++) {
+        int j;
+        j = rand() % n;
+        Bst *node = osSelect(tree, j);
+        std::cout <<j<<"th key number " << i + 1 << ": ";
+        std::cout << node->key << std::endl;
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand(time(nullptr));
+    demo();
     return 0;
 }
 
