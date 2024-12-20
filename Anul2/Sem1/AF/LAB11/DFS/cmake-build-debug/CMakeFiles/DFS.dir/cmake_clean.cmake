edb73679137fd65c9f10c6e7dfce981553c09687
shyines <matei.chira@gmail.com>
2024-12-20 07:08:46 +0200

    .

---
+++ Anul2/Sem1/AF/LAB11/DFS/cmake-build-debug/CMakeFiles/DFS.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DFS.dir/main.cpp.obj"
+  "CMakeFiles/DFS.dir/main.cpp.obj.d"
+  "DFS.exe"
+  "DFS.exe.manifest"
+  "DFS.pdb"
+  "libDFS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Anul2/Sem1/AF/LAB11/DFS/main.cpp
+++ Anul2/Sem1/AF/LAB11/DFS/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <list>
+#include <string.h>
+#include <vector>
+#include "Profiler.h"
 
+Profiler p;
 enum {
     WHITE = 0,
     GREY,
@@ -8,33 +13,160 @@ enum {
 
 struct node {
     int key;
-    int adjSize;
-    node *adj[4500];
+    //int adjSize;
+    std::vector<node*>adj;
     int color;
 };
-
-void dfsVisit(node *start) {
+std::list<node*>list;
+void dfsVisit(node *start, Operation *op) {
     start->color = GREY;
-    for(int i = 0;i < start->adjSize; i++) {
+    if (op != nullptr) {
+        op->count();
+    }
+    for(int i = 0;i < start->adj.size(); i++) {
+        if (op != nullptr) {
+            op->count();
+        }
         if(start->adj[i]->color == WHITE) {
-            dfsVisit(start->adj[i]);
+            dfsVisit(start->adj[i], op);
         }
     }
+    if (op != nullptr) {
+        op->count();
+    }
     start->color = BLACK;
+    if (op == nullptr) {
+        list.emplace_front(start);
+    }
 }
 
-void dfs(node *graph[], int nrOfVertices) {
+void dfs(std::vector<node*> &graph, int nrOfVertices, Operation *op) {
     for(int i = 0; i < nrOfVertices; i++) {
         graph[i]->color = WHITE;
     }
     for(int i = 0;i < nrOfVertices; i++) {
+        if (op != nullptr) {
+            op->count();
+        }
         if(graph[i]->color == WHITE) {
-            dfsVisit(graph[i]);
+            dfsVisit(graph[i], op);
         }
     }
 }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+void addVertice(int from, int to, std::vector<node*> &graph) {
+    graph[from]->adj.push_back(graph[to]);
+    //graph[to]->adj.push_back(graph[from]);
+}
+
+void showGraph(std::vector<node*> &graph, int numOfVertexes) {
+    for (node* it: graph) {
+        std::cout << it->key << ": ";
+        for (node* jt: it->adj) {
+            std::cout << jt->key <<" ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void showList() {
+    for (auto it: list) {
+        std::cout << it->key <<" ";
+    }
+}
+void dfsDemo() {
+    int vertexes[] ={0, 1, 2, 3, 4, 5};
+    int numOfVertexes = sizeof(vertexes) / sizeof(int);
+    std::vector<node*>graph;
+    for (int i = 0;i < numOfVertexes; i++) {
+        node* vertex = new node(vertexes[i]);
+        graph.push_back(vertex);
+    }
+    addVertice(vertexes[5], vertexes[2], graph);
+    addVertice(vertexes[2], vertexes[3], graph);
+    addVertice(vertexes[3], vertexes[1], graph);
+    addVertice(vertexes[4], vertexes[1], graph);
+    addVertice(vertexes[4], vertexes[0], graph);
+
+    showGraph(graph, numOfVertexes);
+    dfs(graph, numOfVertexes, nullptr);
+    showList();
+}
+
+void performance() {
+    Profiler p("dfs");
+    int numOfVertexes = 100;
+    for (int i = 1000;i <= 4500; i+= 100) {
+        std::vector<node*>graph;
+        for (int i = 0;i < numOfVertexes; i++) {
+            node* vertex = new node(i);
+            graph.push_back(vertex);
+        }
+        Operation op = p.createOperation("dfs->edges", i);
+        for (int j = 0;j < i; j++) {
+            int dest = rand() % graph.size();
+            int start = rand() % graph.size();
+            bool ok = false;
+            for (auto nodes: graph[start]->adj) {
+                if (graph[start] == nodes) {
+                    ok = true;
+                    break;
+                }
+            }
+            while (ok) {
+                ok = false;
+                dest = rand() % graph.size();
+                start = rand() % graph.size();
+                for (auto nodes: graph[start]->adj) {
+                    if (graph[dest] == nodes) {
+                        ok = true;
+                        break;
+                    }
+                }
+            }
+            addVertice(start, dest, graph);
+        }
+        dfs(graph, static_cast<int>(graph.size()), &op);
+    }
+
+    for (int i = 100; i <= 200; i+=10) {
+        Operation op = p.createOperation("dfs-vertices", i);
+        std::vector<node*>graph1;
+        for (int j = 0;j < i; j++) {
+            node* vertex = new node(j);
+            graph1.push_back(vertex);
+        }
+        for (int n = 0;n < 4500; n++) {
+            int dest = rand() % graph1.size();
+            int start = rand() % graph1.size();
+            bool ok = false;
+            for (auto nodes: graph1[start]->adj) {
+                if (graph1[start] == nodes) {
+                    ok = true;
+                    break;
+                }
+            }
+            while (ok) {
+                ok = false;
+                dest = rand() % graph1.size();
+                start = rand() % graph1.size();
+                for (auto nodes: graph1[start]->adj) {
+                    if (graph1[dest] == nodes) {
+                        ok = true;
+                        break;
+                    }
+                }
+            }
+            addVertice(start, dest, graph1);
+            dfs(graph1, static_cast<int>(graph1.size()), &op);
+        }
+    }
+    p.showReport();
+}
+
+int main(){
+    dfsDemo();
+    performance();
     return 0;
 }
