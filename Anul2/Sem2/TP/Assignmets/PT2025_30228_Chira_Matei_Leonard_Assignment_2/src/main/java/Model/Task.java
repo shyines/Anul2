86ab8f44887e8a7ed6a87340937df350e57055d5
shyines <matei.chira@gmail.com>
2025-04-02 20:30:09 +0300

    .

---
+++ Anul2/Sem2/TP/Assignmets/PT2025_30228_Chira_Matei_Leonard_Assignment_2/src/main/java/Model/Task.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Task {
+    private int ID;
+    private int arrivalTime;
+    private int serviceTime;
+
+    Task(int ID, int arrivalTime, int serviceTime) {
+        this.ID = ID;
+        this.arrivalTime = arrivalTime;
+        this.serviceTime = serviceTime;
+    }
+
+    
+
+}
